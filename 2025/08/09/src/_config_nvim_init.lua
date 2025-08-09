c6ebb9f424795ab1e4b8a918e337006070f8a4cb
John Smith <johnsmith968530@gmail.com>
2025-08-09 13:25:28 -0700

    Add sample init.lua for neovim

---
+++ 2025/08/09/src/_config_nvim_init.lua
@@ -0,0 +1,13 @@
+-- $Source: /Users/x/Dropbox/2/src/blog/2025/08/09/src/RCS/_config_nvim_init.lua,v $
+-- $Date: 2025/08/09 20:24:42 $
+-- $Revision: 1.1 $
+
+-- Put this file in
+--
+--   ~/.config/nvim/RCS/init.lua
+--
+
+-- Disable mouse entirely
+vim.opt.mouse = ""
+
+-- vim: set et ff=unix ft=lua nocp sts=2 sw=2 ts=2:
---
+++ 2025/08/09/src/setenv.zsh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+# $Source$
+# $Date$
+# $Revision$
+
+
+# vim: set et ff=unix ft=zsh nocp sts=2 sw=2 ts=2:
