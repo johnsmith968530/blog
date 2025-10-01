effdf28e295e2a118549b45c9d8c9892cd275e1c
John Smith <johnsmith968530@gmail.com>
2025-10-01 13:06:47 -0700

    Add polling retries and delay params

--- 2025/10/01/src/wan-2.5.js
+++ 2025/10/01/src/wan-2.5.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
 // $Source: /Users/x/Dropbox/2/src/blog/2025/10/01/src/RCS/wan-2.5.js,v $
-// $Date: 2025/10/01 18:56:44 $
-// $Revision: 1.2 $
+// $Date: 2025/10/01 20:01:47 $
+// $Revision: 1.4 $
 
 import { execSync } from 'child_process';
 const execSyncTrim = (x) => execSync(x, { encoding: 'utf8' }).trim();
@@ -15,6 +15,9 @@ const envyGet = (x) => execSyncTrim(`envy get local nanogpt ${model1} ${x}`);
 const prompt1 = envyGet('prompt');
 const aspect_ratio1 = envyGet('aspect_ratio');
 const duration1 = envyGet('duration');
+const envyGetInt = (x) => parseInt(envyGet(x), 10);
+const retries1 = envyGetInt('polling retries');
+const delay1 = envyGetInt('polling delay');
 
 const filename1 = `${model1}-${stardate1}.mp4`;
 console.log(`Sending ${model1} prompt ${prompt1}`);
@@ -43,16 +46,16 @@ const runId = job.runId;
 // 2) Poll status
 const statusUrl = `${BASE_URL}/generate-video/status?runId=${runId}&modelSlug=${model1}`;
 let videoUrl = null;
-for (let i = 0; i < 120; i++) {
+for (let i = 0; i < retries1; i++) {
   const r = await fetch(statusUrl, { headers: { 'x-api-key': API_KEY } });
   const s = await r.json();
-  console.log('Attempt ' + (i + 1) + ': status=' + (s.data?.status || 'unknown'));
+  console.log('Attempt ' + (i + 1) + ' of ' + retries1 + ': status=' + (s.data?.status || 'unknown'));
   if (s.data?.status === 'COMPLETED') {
     console.log('Completed response:', s);
     videoUrl = s.data?.output?.video?.url;
     break;
   }
-  await new Promise(res => setTimeout(res, 5000));
+  await new Promise(res => setTimeout(res, delay1));
 }
 
 if (videoUrl) {
