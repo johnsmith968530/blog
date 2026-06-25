7ccc5cde6c2546b683f52f96a39368c22d1084c1
John Smith <johnsmith968530@gmail.com>
2026-06-25 12:19:48 -0700

    Prepend today's src dir to PATH

--- 2026/06/25/src/blog.zsh
+++ 2026/06/25/src/blog.zsh
@@ -1,8 +1,9 @@
 # $Source: /Users/x/Dropbox/2/src/blog/2026/06/25/src/RCS/blog.zsh,v $
-# $Date: 2026/06/25 15:41:07 $
-# $Revision: 1.1 $
+# $Date: 2026/06/25 19:15:18 $
+# $Revision: 1.2 $
 
 (( ${+functions[datestamp]} )) || source "$(envy get global blog root)/2026/06/25/src/datestamp.zsh"
+(( ${+functions[prependtopathset]} )) || source "$(envy get global blog root)/2026/06/25/src/prependtopathset.zsh"
 
 blog() {
   local blog1
@@ -12,5 +13,7 @@ blog() {
 }
 
 alias cd.blog='cd "$(blog)"'
+
+prependtopathset PATH "$(blog)/src"
 
 # vim: set et ff=unix ft=zsh nocp sts=2 sw=2 ts=2:
